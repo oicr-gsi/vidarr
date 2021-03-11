46888cd7c46bee89b2599fe52a73c35c3f837f59
Alexis Varsava <alexis.varsava@oicr.on.ca>
2021-03-11 16:56:42 -0500

    set up ssh? modules r mad tho

--- vidarr-niassa/src/main/java/ca/on/oicr/gsi/vidarr/niassa/NiassaOutputProvisionerProvider.java
+++ vidarr-niassa/src/main/java/ca/on/oicr/gsi/vidarr/niassa/NiassaOutputProvisionerProvider.java
@@ -5,6 +5,8 @@ import ca.on.oicr.gsi.vidarr.OutputProvisionerProvider;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.io.IOException;
+
 import static ca.on.oicr.gsi.vidarr.niassa.NiassaOutputProvisioner.MAPPER;
 
 public class NiassaOutputProvisionerProvider implements OutputProvisionerProvider {
@@ -14,9 +16,11 @@ public class NiassaOutputProvisionerProvider implements OutputProvisionerProvide
             return new NiassaOutputProvisioner(
                     node.has("chunks") ? MAPPER.treeToValue(node.get("chunks"), int[].class) : new int[0],
                     node.get("username").asText(),
-                    node.get("password").asText(),
-                    node.get("hostname").asText());
+                    node.get("hostname").asText(),
+                    node.get("port").shortValue());
         } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }

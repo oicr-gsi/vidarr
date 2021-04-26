91de503c7e60d88e6ce57f9cb991e4dfc411ad9f
Alexis Varsava <alexis.varsava@oicr.on.ca>
2021-04-26 10:20:59 -0400

    Try convertValue

--- vidarr-niassa/src/main/java/ca/on/oicr/gsi/vidarr/niassa/NiassaWorkflowEngineProvider.java
+++ vidarr-niassa/src/main/java/ca/on/oicr/gsi/vidarr/niassa/NiassaWorkflowEngineProvider.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import java.lang.reflect.Type;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,17 +15,13 @@ import static ca.on.oicr.gsi.vidarr.niassa.NiassaWorkflowEngine.MAPPER;
 public class NiassaWorkflowEngineProvider implements WorkflowEngineProvider {
     @Override
     public WorkflowEngine readConfiguration(ObjectNode node) {
-        try {
-            return new NiassaWorkflowEngine(node.get("dbUrl").asText(),
-                    node.get("dbUser").asText(),
-                    node.get("dbPass").asText(),
-                    // VERY unclear whether this will work
-                    node.has("annotations")?
-                            MAPPER.readValue(node.get("annotations").asText(), new TypeReference<HashSet<String>>(){})
-                            : new HashSet<>());
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return new NiassaWorkflowEngine(node.get("dbUrl").asText(),
+                node.get("dbUser").asText(),
+                node.get("dbPass").asText(),
+                // VERY unclear whether this will work
+                node.has("annotations")?
+                        MAPPER.convertValue(node.get("annotations"), new TypeReference<>() {})
+                        : new HashSet<>());
     }
 
     @Override

a7a297bba39926fbc7bb2f9a84b80f658a320032
Heather Armstrong <heather.armstrong@oicr.on.ca>
2021-09-07 16:02:48 -0400

    Minor fix

--- vidarr-server/src/main/java/ca/on/oicr/gsi/vidarr/server/DatabaseBackedProcessor.java
+++ vidarr-server/src/main/java/ca/on/oicr/gsi/vidarr/server/DatabaseBackedProcessor.java
@@ -190,13 +190,13 @@ public abstract class DatabaseBackedProcessor
       Iterable<String> labels,
       TreeSet<String>
           inputIds, // In both existing calls, hashFromAnalysisId has already been called. TODO:
-                    // maybe refactor if any more calls are needed
+      // maybe refactor if any more calls are needed
       Collection<? extends ExternalId> externalIds) {
     try {
       final var digest = MessageDigest.getInstance("SHA-256");
       digest.update(name.getBytes(StandardCharsets.UTF_8));
       for (final var id : inputIds) {
-        final var idBytes = id.getBytes(StandardCharsets.UTF_8);
+        final var idBytes = hashFromAnalysisId(id).getBytes(StandardCharsets.UTF_8);
         digest.update(new byte[] {0});
         digest.update(idBytes);
       }

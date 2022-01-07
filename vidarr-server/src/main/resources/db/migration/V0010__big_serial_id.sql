5f3a785f8e6698c02e2e3886a7f6befb42b5ff42
Alexis Varsava <alexis.varsava@oicr.on.ca>
2022-01-07 08:41:34 -0500

    Clear flyway complaint

--- vidarr-server/src/main/resources/db/migration/V0010__big_serial_id.sql
+++ vidarr-server/src/main/resources/db/migration/V0010__big_serial_id.sql
@@ -3,4 +3,3 @@ ALTER TABLE active_workflow_run ALTER COLUMN id TYPE bigint;
 ALTER TABLE active_operation ALTER COLUMN workflow_run_id TYPE bigint;
 ALTER TABLE external_id ALTER COLUMN workflow_run_id TYPE bigint;
 ALTER TABLE analysis ALTER COLUMN workflow_run_id TYPE bigint;
-ALTER TABLE active_operation ALTER COLUMN id TYPE bigint;

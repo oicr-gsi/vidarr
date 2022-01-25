406e66aeed9f6bb001c246596dd8ec40f4862c76
Heather Armstrong <heather.armstrong@oicr.on.ca>
2022-01-25 15:09:58 -0500

    Fix the getIdsForDownstreamWorkflowRuns function in response to testing it out on data copied from production.

--- vidarr-server/src/main/resources/db/migration/V0012__add_get_ids_for_downstream_workflow_runs_function.sql
+++ vidarr-server/src/main/resources/db/migration/V0012__add_get_ids_for_downstream_workflow_runs_function.sql
@@ -4,10 +4,11 @@
     CREATE OR REPLACE FUNCTION get_ids_for_downstream_workflow_runs(wr_ids BIGINT[]) RETURNS table (wfr_id bigint) AS $$
       BEGIN
         RETURN QUERY
-        SELECT DISTINCT id FROM workflow_run
-                WHERE array_length(input_file_ids, 1) > 0 AND
-                  -- workflow uses files produced by target workflow run as its input
-                  (SELECT ARRAY(SELECT DISTINCT SPLIT_PART(UNNEST(input_file_ids), '/', 3)::VARCHAR FROM workflow_run)) <@ (
-                    SELECT ARRAY(SELECT DISTINCT hash_id FROM analysis WHERE ARRAY[workflow_run_id] <@ wr_ids));
+        SELECT DISTINCT a.id FROM
+          (SELECT id, ARRAY(SELECT SPLIT_PART(UNNEST(input_file_ids), '/', 3)::VARCHAR) AS input_file_ids
+          FROM workflow_run) AS a
+        WHERE cardinality(a.input_file_ids) > 0 AND a.input_file_ids && (
+          SELECT ARRAY(SELECT DISTINCT hash_id FROM analysis WHERE ARRAY[workflow_run_id] <@ wr_ids));
+          -- note: the `&&` is the array intersection operator, whereas `[A] <@ [B]` is "all elements in A are present in B"
       END;
       $$ LANGUAGE plpgsql;

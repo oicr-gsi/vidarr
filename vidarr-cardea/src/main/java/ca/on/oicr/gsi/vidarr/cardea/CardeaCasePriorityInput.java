76b6e6753f7e88c0a436490df9a7339498402b75
Alexis Varsava <alexis.varsava@oicr.on.ca>
2025-08-07 11:21:04 -0400

    Handle blank caseId

--- vidarr-cardea/src/main/java/ca/on/oicr/gsi/vidarr/cardea/CardeaCasePriorityInput.java
+++ vidarr-cardea/src/main/java/ca/on/oicr/gsi/vidarr/cardea/CardeaCasePriorityInput.java
@@ -92,6 +92,9 @@ public class CardeaCasePriorityInput implements PriorityInput {
         new KeyValueCache<>(resourceName + " " + inputName, ttl, SimpleRecord::new) {
           @Override
           protected Optional<Integer> fetch(String caseId, Instant lastUpdated) throws Exception {
+            if (caseId.isBlank()) {
+              return Optional.of(defaultPriority);
+            }
             String fullUrl = new StringBuilder(casesUrl)
                 .append(URLEncoder.encode(caseId, StandardCharsets.UTF_8)
                     .replace("+", "%20"))

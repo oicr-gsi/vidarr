9b5ad0e3094bd452d72ee8746268f82c74948e8b
Heather Armstrong <harmstrong@oicr.on.ca>
2025-11-04 17:10:22 -0500

    fixup - no need for all this, it will do all the releasing in the CRChecker once it's marked as not-live in the delete function

--- vidarr-server/src/main/java/ca/on/oicr/gsi/vidarr/server/Main.java
+++ vidarr-server/src/main/java/ca/on/oicr/gsi/vidarr/server/Main.java
@@ -1147,7 +1147,6 @@ public final class Main implements ServerConfig {
     exchange.setStatusCode(
         processor.delete(
             vidarrId,
-            targets,
             new DeleteResultHandler<>() {
               @Override
               public Integer deleted() {
@@ -2518,10 +2517,6 @@ public final class Main implements ServerConfig {
                 UnloadResponse res = new UnloadResponse();
                 res.setFilename(filename);
                 res.setDeletedWorkflowRuns(hashes);
-                // TODO: when we implement unloading queued workflow runs, their consumable
-                // resources will also need to be released with
-                // processor.releaseConsumableResources()
-
                 epoch = time.toEpochMilli();
                 return res;
               });

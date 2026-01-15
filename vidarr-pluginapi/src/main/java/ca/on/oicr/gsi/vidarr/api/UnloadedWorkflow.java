fdf95557717b50248785866a05d19bf50ff5c61d
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-01-15 15:57:39 -0500

    equals and hashcodes for some API classes

--- vidarr-pluginapi/src/main/java/ca/on/oicr/gsi/vidarr/api/UnloadedWorkflow.java
+++ vidarr-pluginapi/src/main/java/ca/on/oicr/gsi/vidarr/api/UnloadedWorkflow.java
@@ -3,6 +3,7 @@ package ca.on.oicr.gsi.vidarr.api;
 import ca.on.oicr.gsi.vidarr.BasicType;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.Map;
+import java.util.Objects;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public final class UnloadedWorkflow {
@@ -24,5 +25,19 @@ public final class UnloadedWorkflow {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  @Override
+  public boolean equals(Object o){
+    if (this == o) return true;
+    if (null == o || this.getClass() != o.getClass()) return false;
+    UnloadedWorkflow other = (UnloadedWorkflow) o;
+    return Objects.equals(this.labels, other.labels)
+        && Objects.equals(this.name, other.name);
+  }
+
+  @Override
+  public int hashCode(){
+    return Objects.hash(labels, name);
   }
 }

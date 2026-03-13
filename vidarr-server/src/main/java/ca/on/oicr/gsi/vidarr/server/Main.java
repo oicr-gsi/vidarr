973618598629699879030bb3a5c2ca75761422f6
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-03-13 10:45:08 -0400

    Clarify documentation

--- vidarr-server/src/main/java/ca/on/oicr/gsi/vidarr/server/Main.java
+++ vidarr-server/src/main/java/ca/on/oicr/gsi/vidarr/server/Main.java
@@ -2137,7 +2137,7 @@ public final class Main implements ServerConfig {
           return;
         }
 
-        // 2. Can't have a duplicate workflow version
+        // 2. Can't have duplicate workflow versions in the request
         if (info.second().containsKey(workflowVersion.getVersion())) {
           badRequestResponse(
               exchange,
@@ -2170,7 +2170,7 @@ public final class Main implements ServerConfig {
       for (final ProvenanceWorkflowRun<ExternalMultiVersionKey> workflowRun :
           unloadedData.getWorkflowRuns()) {
 
-        // All workflow runs must be of installed workflows
+        // All workflow runs must be of included workflows
         final Pair<UnloadedWorkflow, Map<String, Pair<String, UnloadedWorkflowVersion>>> info =
             workflowInfo.get(workflowRun.getWorkflowName());
         if (info == null) {
@@ -2182,7 +2182,7 @@ public final class Main implements ServerConfig {
           return;
         }
 
-        // All workflow runs must be of installed workflow versions
+        // All workflow runs must be of included workflow versions
         final Pair<String, UnloadedWorkflowVersion> versionInfo =
             info.second().get(workflowRun.getWorkflowVersion());
         if (versionInfo == null) {

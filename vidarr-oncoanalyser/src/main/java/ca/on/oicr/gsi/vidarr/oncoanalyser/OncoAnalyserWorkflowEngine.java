affe3986513e5cb561456429f2acf0b02db437f9
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-07-21 15:54:02 -0400

    Add bucket config

--- vidarr-oncoanalyser/src/main/java/ca/on/oicr/gsi/vidarr/oncoanalyser/OncoAnalyserWorkflowEngine.java
+++ vidarr-oncoanalyser/src/main/java/ca/on/oicr/gsi/vidarr/oncoanalyser/OncoAnalyserWorkflowEngine.java
@@ -3,7 +3,11 @@ package ca.on.oicr.gsi.vidarr.oncoanalyser;
 import ca.on.oicr.gsi.Pair;
 import ca.on.oicr.gsi.status.SectionRenderer;
 import ca.on.oicr.gsi.vidarr.*;
+import tools.jackson.databind.DeserializationFeature;
 import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.SerializationFeature;
+import tools.jackson.databind.cfg.DateTimeFeature;
+import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.databind.node.ObjectNode;
 
 import javax.xml.stream.XMLStreamException;
@@ -16,6 +20,15 @@ public class OncoAnalyserWorkflowEngine implements WorkflowEngine<StateUnstarted
         return () -> Stream.of(new Pair<>("oncoanalyser", OncoAnalyserWorkflowEngine.class));
     }
 
+    static final JsonMapper MAPPER =
+            JsonMapper.builder()
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    .configure(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS, true)
+                    .configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+                    .build();
+
+    private String statusBucket, metadataBucket, manifestBucket;
+
     @Override
     public OperationAction<?, CleanupState, Void> cleanup() {
         return null;
@@ -23,7 +36,9 @@ public class OncoAnalyserWorkflowEngine implements WorkflowEngine<StateUnstarted
 
     @Override
     public void configuration(SectionRenderer sectionRenderer) throws XMLStreamException {
-
+        sectionRenderer.line("Bucket containing status.json", statusBucket);
+        sectionRenderer.line("Bucket containing metadata.json", metadataBucket);
+        sectionRenderer.line("Bucket containing manifest.json", manifestBucket);
     }
 
     @Override
@@ -49,5 +64,17 @@ public class OncoAnalyserWorkflowEngine implements WorkflowEngine<StateUnstarted
     @Override
     public boolean supports(WorkflowLanguage language) {
         return false;
+    }
+
+    public void setStatusBucket(String statusBucket) {
+        this.statusBucket = statusBucket;
+    }
+
+    public void setMetadataBucket(String metadataBucket) {
+        this.metadataBucket = metadataBucket;
+    }
+
+    public void setManifestBucket(String manifestBucket) {
+        this.manifestBucket = manifestBucket;
     }
 }
--- vidarr-oncoanalyser/src/main/java/module-info.java
+++ vidarr-oncoanalyser/src/main/java/module-info.java
@@ -4,6 +4,7 @@ import ca.on.oicr.gsi.vidarr.oncoanalyser.OncoAnalyserWorkflowEngine;
 module vidarr.oncoanalyser {
     requires ca.on.oicr.gsi.vidarr.pluginapi;
     requires java.xml;
+    requires tools.jackson.databind;
 
     provides WorkflowEngineProvider
             with OncoAnalyserWorkflowEngine;

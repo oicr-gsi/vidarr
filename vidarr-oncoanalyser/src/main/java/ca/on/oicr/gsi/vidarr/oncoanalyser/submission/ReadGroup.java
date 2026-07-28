feb8ca3f00ce280530c2c4adaec6564888085870
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-07-28 08:43:10 -0400

    typo

--- vidarr-oncoanalyser/src/main/java/ca/on/oicr/gsi/vidarr/oncoanalyser/submission/ReadGroup.java
+++ vidarr-oncoanalyser/src/main/java/ca/on/oicr/gsi/vidarr/oncoanalyser/submission/ReadGroup.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 
 /**
  * OncoAnalyser does not require any specific combination of ReadGroup fields; all combinations, including
- * none set, are valid. It is an OICR requirement that ID, DM, LB, PU, and CN be set.
+ * none set, are valid. It is an OICR requirement that ID, SM, LB, PU, and CN be set.
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ReadGroup(String ID, String BC, String CN, String DS, String DT, String FO, String KS, String LB,

5f2a447e2fbb82c596fc1fa8a82746b3ed0189a6
Alexis Varsava <alexis.varsava@oicr.on.ca>
2026-07-28 08:31:43 -0400

    Explain the ReadGroup requirements are ours

--- vidarr-oncoanalyser/src/main/java/ca/on/oicr/gsi/vidarr/oncoanalyser/submission/ReadGroup.java
+++ vidarr-oncoanalyser/src/main/java/ca/on/oicr/gsi/vidarr/oncoanalyser/submission/ReadGroup.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Objects;
 
+/**
+ * OncoAnalyser does not require any specific combination of ReadGroup fields; all combinations, including
+ * none set, are valid. It is an OICR requirement that ID, DM, LB, PU, and CN be set.
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record ReadGroup(String ID, String BC, String CN, String DS, String DT, String FO, String KS, String LB,
                         String PG, Integer PI, String PL, String PM, String PU, String SM) {
